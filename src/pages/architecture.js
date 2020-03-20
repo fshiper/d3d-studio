710d2d93caf32336fbd3e31d4abc8d37d2ac6f82
fshiper <przemyslaw.krzywodajc@gmail.com>
2020-03-20 13:42:35 +0100

    pano styling

--- src/pages/architecture.js
+++ src/pages/architecture.js
@@ -15,6 +15,9 @@ const ArchitecturePage = () => {
     width: "900px",
     height: "600px",
     margin: "auto",
+    "border-radius": "0.375rem",
+    "box-shadow":
+      "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
   }
 
   return (
